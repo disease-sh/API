d714780fdb13e62e4b98dd6a00512e325a2fe729
dicedtomato <35403473+dicedtomatoreal@users.noreply.github.com>
2020-03-18 20:16:20 -0700

    add redis, and made everything in config.json!

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var cors = require("cors");
 var axios = require("axios");
 var cheerio = require("cheerio");
-var db = require("quick.db");
+var cors = require('cors');
+const config = require('./config.json');
+const Redis = require('ioredis');
 
-var getall = setInterval(async () => {
+app.use(cors());
+
+// create redis instance :O
+const redis = new Redis(config.redis.host, {
+  password: config.redis.password
+})
+
+const keys = config.keys
+
+
+var getall = async () => {
   let response;
   try {
     response = await axios.get("https://www.worldometers.info/coronavirus/");
@@ -15,10 +26,8 @@ var getall = setInterval(async () => {
   } catch (err) {
     return null;
   }
-
   // to store parsed data
   const result = {};
-
   // get HTML and parse death rates
   const html = cheerio.load(response.data);
   html(".maincounter-number").filter((i, el) => {
@@ -33,12 +42,12 @@ var getall = setInterval(async () => {
       result.recovered = count;
     }
   });
-  result.updated = Date.now();
- db.set("all", result);
+  result.updated = Date.now()
+  const string = JSON.stringify(result);
+  redis.set(keys.all, string);
   console.log("Updated The Cases", result);
-}, 600000);
-
-var getcountries = setInterval(async () => {
+}
+var getcountries = async () => {
   let response;
   try {
     response = await axios.get("https://www.worldometers.info/coronavirus/");
@@ -48,10 +57,8 @@ var getcountries = setInterval(async () => {
   } catch (err) {
     return null;
   }
-
   // to store parsed data
   const result = [];
-
   // get HTML and parse death rates
   const html = cheerio.load(response.data);
   const countriesTable = html("table#main_table_countries_today");
@@ -59,7 +66,6 @@ var getcountries = setInterval(async () => {
     .children("tbody")
     .children("tr")
     .children("td");
-
   // NOTE: this will change when table format change in website
   const totalColumns = 9;
   const countryColIndex = 0;
@@ -68,8 +74,9 @@ var getcountries = setInterval(async () => {
   const deathsColIndex = 3;
   const todayDeathsColIndex = 4;
   const curedColIndex = 5;
+  const activeColIndex = 6;
   const criticalColIndex = 7;
-
+  const casesPerOneMillionColIndex = 8;
   // minus totalColumns to skip last row, which is total
   for (let i = 0; i < countriesTableCells.length - totalColumns; i += 1) {
     const cell = countriesTableCells[i];
@@ -92,7 +99,7 @@ var getcountries = setInterval(async () => {
     }
     // get cases
     if (i % totalColumns === casesColIndex) {
-      let cases = cell.children[0].data || "";
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].cases = parseInt(
         cases.trim().replace(/,/g, "") || "0",
         10
@@ -100,7 +107,7 @@ var getcountries = setInterval(async () => {
     }
     // get today cases
     if (i % totalColumns === todayCasesColIndex) {
-      let cases = cell.children[0].data || "";
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].todayCases = parseInt(
         cases.trim().replace(/,/g, "") || "0",
         10
@@ -108,7 +115,7 @@ var getcountries = setInterval(async () => {
     }
     // get deaths
     if (i % totalColumns === deathsColIndex) {
-      let deaths = cell.children[0].data || "";
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].deaths = parseInt(
         deaths.trim().replace(/,/g, "") || "0",
         10
@@ -116,7 +123,7 @@ var getcountries = setInterval(async () => {
     }
     // get today deaths
     if (i % totalColumns === todayDeathsColIndex) {
-      let deaths = cell.children[0].data || "";
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].todayDeaths = parseInt(
         deaths.trim().replace(/,/g, "") || "0",
         10
@@ -124,78 +131,78 @@ var getcountries = setInterval(async () => {
     }
     // get cured
     if (i % totalColumns === curedColIndex) {
-      let cured = cell.children[0].data || 0;
+      let cured = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].recovered = parseInt(
+        cured.trim().replace(/,/g, "") || 0,
+        10
+      );
+    }
+    // get active
+    if (i % totalColumns === activeColIndex) {
+      let cured = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].active = parseInt(
         cured.trim().replace(/,/g, "") || 0,
         10
       );
     }
     // get critical
     if (i % totalColumns === criticalColIndex) {
-      let critical = cell.children[0].data || "";
+      let critical = cell.children.length != 0 ? cell.children[0].data : "";
       result[result.length - 1].critical = parseInt(
         critical.trim().replace(/,/g, "") || "0",
         10
       );
     }
-  }
-  db.set("countries", result);
-  console.log("Updated The Countries", result);
-}, 600000);
-
-app.get("/", async function(request, response) {
-  let a = await db.fetch("all");
-  response.send(
-    `${a.cases} cases are reported of the COVID-19 Novel Coronavirus strain<br> ${a.deaths} have died from it <br>\n${a.recovered} have recovered from it <br> Get the endpoint /all to get information for all cases <br> get the endpoint /countries for getting the data sorted country wise <br>get the endpoint /countries/[country-name] for getting the data for a specific country`
-  );
-});
-
-var listener = app.listen(80, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-app.use(cors());
-
-app.get("/all/", async function(req, res) {
-  let all = await db.fetch("all");
-  res.send(all);
-});
-
-app.get("/invite/", async function(req, res) {
-  res.redirect("https://discordapp.com/oauth2/authorize?client_id=685268214435020809&scope=bot&permissions=537250880")
-});
-
-app.get("/countries/", async function(req, res) {
-  let countries = await db.fetch("countries");
-  if(req.query['sort']) {
-    try {
-      const sortProp = req.query['sort'];
-      countries.sort((a,b) => {
-        if(a[sortProp] < b[sortProp]) {
-          return -1;
-        }
-        else if(a[sortProp] > b[sortProp]) {
-          return 1;
-        }
-        return 0;
-      })
-    } catch(e) {
-      console.error("ERROR while sorting", e);
-      res.status(422).send(e);
-      return;
+    // get total cases per one million population
+    if (i % totalColumns === casesPerOneMillionColIndex) {
+      let casesPerOneMillion = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].casesPerOneMillion = parseInt(
+        casesPerOneMillion.trim().replace(/,/g, "") || "0",
+        10
+      );
     }
   }
+
+  const string = JSON.stringify(result);
+  redis.set(keys.countries, string);
+  console.log(`Updated countries: ${result.length} countries`);
+};
+
+getcountries()
+getall()
+
+setInterval(getcountries, config.interval);
+setInterval(getall, config.interval);
+
+app.get("/", async function (request, response) {
+  let a = JSON.parse(await redis.get(keys.all))
+  response.send(
+    `${a.cases} cases are reported of the COVID-19 Novel Coronavirus strain<br> ${a.deaths} have died from it <br>\n${a.recovered} have recovered from it <br> Get the endpoint /a
+ll to get information for all cases <br> get the endpoint /countries for getting the data sorted country wise`
+  );
+});
+var listener = app.listen(config.port, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+app.get("/all/", async function (req, res) {
+  let all = JSON.parse(await redis.get(keys.all))
+  res.send(all);
+});
+app.get("/countries/", async function (req, res) {
+  let countries = JSON.parse(await redis.get(keys.countries))
   res.send(countries);
 });
-
-app.get("/countries/:country", async function(req, res) {
-  let countries = await db.fetch("countries");
+app.get("/countries/:country", async function (req, res) {
+  let countries = JSON.parse(await redis.get(keys.countries))
   let country = countries.find(
-    e => e.country.toLowerCase().includes(req.params.country.toLowerCase()) // Added this so people dnt have to put the whole country name :)
+    e => e.country.toLowerCase().includes(req.params.country.toLowerCase())
   );
   if (!country) {
     res.send("Country not found");
     return;
   }
   res.send(country);
+});
+app.get("/invite/", async function (req, res) {
+  res.redirect("https://discordapp.com/oauth2/authorize?client_id=685268214435020809&scope=bot&permissions=537250880")
 });
