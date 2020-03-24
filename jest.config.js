548a5a52749c813ef8bedcc5236cecf8395188f9
Jesse Stuart <hi@jessestuart.com>
2020-03-24 13:40:45 -0400

    feat(ci): Add circleci integration.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
   coverageDirectory: 'tests/coverage/',
   moduleDirectories: ['src', 'node_modules'],
   moduleFileExtensions: ['js', 'json'],
-  reporters: ['default', 'jest-junit'],
+  reporters: ['default', ['jest-junit', {
+    outputDirectory: 'tests/reports/jest/',
+    outputName: 'results.xml',
+  }]],
   testEnvironment: 'node',
   testRegex: '.*(test|spec)\\.js$',
 }
