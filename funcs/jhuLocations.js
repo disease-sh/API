65aebe4a288bffb6bdd019eae721952cf90bfcb8
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-24 17:56:07 -0500

    remove city field and make US states have one entry

--- funcs/jhuLocations.js
+++ funcs/jhuLocations.js
@@ -58,4 +58,90 @@ var jhudata = async (keys, redis) => {
   console.log(`Updated JHU CSSE: ${result.length} locations`);
 };
 
-module.exports = jhudata;
+var jhudata_v2 = async (keys, redis) => {
+  let response;
+  const date = new Date();
+  try {
+    response = await axios.get(
+      `${base}0${date.getMonth() +
+        1}-${date.getDate()}-${date.getFullYear()}.csv`
+    );
+    console.log(
+      `USING 0${date.getMonth() + 1}-${date.getDate() -
+        1}-${date.getFullYear()}.csv CSSEGISandData`
+    );
+  } catch (err) {
+    response = await axios.get(
+      `${base}0${date.getMonth() + 1}-${date.getDate() -
+        1}-${date.getFullYear()}.csv`
+    );
+    console.log(
+      `USING 0${date.getMonth() + 1}-${date.getDate() -
+        1}-${date.getFullYear()}.csv CSSEGISandData`
+    );
+  }
+
+  const parsed = await csv({
+    noheader: true,
+    output: "csv"
+  }).fromString(response.data);
+
+  // to store parsed data
+  const result = [];
+  const statesResult = {};
+
+  for (const loc of parsed.splice(1)) {
+    // city exists only for US entries
+    if (loc[1] != "") {
+      if (statesResult[loc[2]]) {
+        // sum
+        statesResult[loc[2]].stats.confirmed += parseInt(loc[7]);
+        statesResult[loc[2]].stats.deaths += parseInt(loc[8]);
+        statesResult[loc[2]].stats.recovered += parseInt(loc[9]);
+      }
+      else {
+        // initialize
+        statesResult[loc[2]] = {
+          country: loc[3],
+          province: loc[2] === "" ? null : loc[2],
+          updatedAt: loc[4],
+          stats: {
+            confirmed: parseInt(loc[7]),
+            deaths: parseInt(loc[8]),
+            recovered: parseInt(loc[9])
+          },
+          coordinates: {
+            latitude: loc[5],
+            longitude: loc[6]
+          }
+        }
+      }
+    }
+    else {
+      result.push({
+        country: loc[3],
+        province: loc[2] === "" ? null : loc[2],
+        updatedAt: loc[4],
+        stats: {
+          confirmed: parseInt(loc[7]),
+          deaths: parseInt(loc[8]),
+          recovered: parseInt(loc[9])
+        },
+        coordinates: {
+          latitude: loc[5],
+          longitude: loc[6]
+        }
+      });
+    }
+  }
+  // add US entries
+  Object.keys(statesResult).map((state, index) => result.push(statesResult[state]));
+  const string = JSON.stringify(result);
+  redis.set(keys.jhu_v2, string);
+  console.log(`Updated JHU CSSE: ${result.length} locations`);
+};
+
+module.exports = {
+  jhudata,
+  jhudata_v2
+};
