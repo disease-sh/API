9382b46bda77f28461a8e4dc7926bb9a082db9a7
Mr Cerberus <61522268+Mr-Cerberus@users.noreply.github.com>
2020-03-25 16:36:36 +0000

    Updated to work with getYesterday's PR

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 module.exports = {
     getAll: require('./funcs/getAll'),
     getCountries: require('./funcs/getCountries'),
+    getYesterday: require('./funcs/getYesterday'),
     getStates: require('./funcs/getStates'),
     jhuLocations: require('./funcs/jhuLocations'),
     historical: require('./funcs/historical'),
