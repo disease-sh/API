9996bd21012cc661b02dba613f077245aa3024d9
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-28 19:31:01 +0000

    Added Swagger/OpenAPI

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { RootModule } from './modules/root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('covid19')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
