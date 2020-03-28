01b1133d1a50d9782e8f2640d3f24ce6b5b6bce8
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-27 23:38:14 -0500

    refactor all endpt

--- funcs/getAll.js
+++ funcs/getAll.js
@@ -1,21 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const getall = async (keys, redis) => {
-	let response;
-	try {
-		response = await axios.get('https://www.worldometers.info/coronavirus/');
-		if (response.status !== 200) {
-			return console.log('ERROR');
-		}
-	} catch (err) {
-		return null;
-	}
-	// to store parsed data
+function fillResult(html) {
 	const result = {};
-	// get HTML and parse death rates
-	const html = cheerio.load(response.data);
-
 	// eslint-disable-next-line array-callback-return
 	html('.maincounter-number').map((i, el) => {
 		let count = el.children[0].next.children[0].data || '0';
@@ -31,6 +18,23 @@ const getall = async (keys, redis) => {
 	});
 	result.updated = Date.now();
 	result.active = result.cases - result.deaths - result.recovered;
+	return result;
+}
+
+const getall = async (keys, redis) => {
+	let response;
+	try {
+		response = await axios.get('https://www.worldometers.info/coronavirus/');
+		if (response.status !== 200) {
+			return console.log('ERROR');
+		}
+	} catch (err) {
+		return null;
+	}
+
+	// get HTML and parse death rates
+	const html = cheerio.load(response.data);
+	const result = fillResult(html);
 	const string = JSON.stringify(result);
 	redis.set(keys.all, string);
 	return console.log('Updated The Cases', result);
