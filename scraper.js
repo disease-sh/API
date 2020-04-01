81adfe45b3988899c03a8a365a798d282d9a2081
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-01 10:52:50 -0500

    fix merge conflicts

--- scraper.js
+++ scraper.js
@@ -6,19 +6,9 @@ const historical = require('./funcs/historical');
 
 
 module.exports = {
-<<<<<<< HEAD
-    getAll: require('./funcs/getAll'),
-    getCountries: require('./funcs/getCountries'),
-    getYesterday: require('./funcs/getYesterday'),
-    getStates: require('./funcs/getStates'),
-    jhuLocations: require('./funcs/jhuLocations'),
-    historical: require('./funcs/historical'),
-}
-=======
 	getAll,
 	getWorldometers,
 	getStates,
 	jhuLocations,
 	historical
 };
->>>>>>> 53ee8dbf02c9a8fec0108c56208f73522cb4c66e
