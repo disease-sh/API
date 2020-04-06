c3b5466999b1281c33174d38628c266b357dbd97
Nicolas Le Borgne <nicolas.leborgne@me.com>
2020-04-06 15:49:54 +1000

    Fixed Deepscan Medium issues regarding forEach loop for getCountryName and getCountryCode

--- utils/country_utils.js
+++ utils/country_utils.js
@@ -9,13 +9,12 @@ const countryData = require('./countries.json');
  * @returns {string}
  */
 const getCountryCode = (countryName) => {
-	countryData.forEach((element) => {
+	for(var element of countryData) {
 		if (element.country.toLowerCase() === countryName.toLowerCase()) {
 			return element.iso2;
 		}
-		return true;
-	});
-	return null;
+		return null;
+	}
 };
 
 /**
@@ -24,13 +23,14 @@ const getCountryCode = (countryName) => {
  * @returns {string}
  */
 const getCountryName = (countryCode) => {
-	countryData.forEach((element) => {
+
+	for(var element of countryData) {
 		if (element.iso2.toLowerCase() === countryCode.toLowerCase()) {
 			return element.country;
 		}
-		return true;
-	});
-	return null;
+		return null;
+	}
+
 };
 
 const getCountryData = (countryNameParam) => {
