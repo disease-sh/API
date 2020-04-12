0c571c9fad5c1116e5d2b1ffb5511c382a7e8f1d
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-12 11:14:41 -0500

    add jsdoc

--- scrapers/getStates.js
+++ scrapers/getStates.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const logger = require('../utils/logger');
 
+/**
+ * Get state name from html table cell
+ * @param 	{Object} 	cell 	Table cell from states table
+ * @returns {string} 			State name
+ */
 const parseStateCell = (cell) => {
 	let state = cell.children[0].data
 		|| cell.children[0].children[0].data
@@ -16,11 +21,22 @@ const parseStateCell = (cell) => {
 	return state.trim() || '';
 };
 
+/**
+ * Gets a data vlue, example cases, from an html table cell
+ * @param 	{Object} 	cell 	Table cell from states table
+ * @returns {float} 			Value of cell
+ */
 const parseNumberCell = (cell) => {
 	const cellValue = cell.children.length !== 0 ? cell.children[0].data : '';
 	return parseFloat(cellValue.replace(/[,+\-\s]/g, '')) || 0;
 };
 
+/**
+ * Scrapes state table data from worldometers
+ * @param 	{Object} 	html 		Html parsed from worldometers
+ * @param 	{boolean} 	yesterday 	Indicates whether data should be scraped for yesterday or today
+ * @returns {Array} 				Returns array of data for each state with cases, deaths, etc
+ */
 const fillResult = (html, yesterday = false) => {
 	const statesTable = html(yesterday ? 'table#usa_table_countries_yesterday' : 'table#usa_table_countries_today');
 	const tableRows = statesTable
@@ -48,6 +64,11 @@ const fillResult = (html, yesterday = false) => {
 	});
 };
 
+/**
+ * Fills redis with states and yesterday_states data
+ * @param 	{string} 	keys	 Redis keys
+ * @param 	{Object} 	redis 	 Redis instance
+ */
 const getStates = async (keys, redis) => {
 	let response;
 	try {
