2773e781dab951c94d5687d7bbf8363aad9d6eb5
Julian Pufler <puf17640@spengergasse.at>
2020-04-14 19:47:21 +0200

    refactored logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,7 @@
-const httpErrorLogger = (err, message = 'Not message loaded') => console.log({
-	message,
-	errno: err.errno,
-	url: err.config.url
-});
-
 module.exports = {
-	httpErrorLogger
+	httpErrorLogger: (err, message = 'Message could not be loaded') => console.error({
+		message,
+		errno: err.errno,
+		url: err.config.url
+	})
 };
