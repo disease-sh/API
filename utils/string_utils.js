4b4c10d28ff775b48df6c73d3f558350453125eb
Julian Pufler <puf17640@spengergasse.at>
2020-04-14 19:47:06 +0200

    refactored string utils

--- utils/string_utils.js
+++ utils/string_utils.js
@@ -1,21 +1,5 @@
-const wordsStandardize = (word) => {
-	if (word) {
-		const wordStandard = word.trim().toLowerCase();
-		return wordStandard.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-	}
-	return 'null';
-};
-
-const wordToBoolean = word => {
-	if (word === '') return true;
-	if (word) return word.toLowerCase() === 'true' || word === '1';
-	return false;
-};
-
-const splitQuery = (query) => query.indexOf('|') === -1 ? query.split(',') : query.split('|');
-
 module.exports = {
-	wordsStandardize,
-	wordToBoolean,
-	splitQuery
+	wordsStandardize: (word) => word ? wordStandard.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') : 'null',
+	wordToBoolean: (word) => word === '' || (word ? word.toLowerCase() === 'true' || word === '1' : false),
+	splitQuery: (query) => query.indexOf('|') === -1 ? query.split(',') : query.split('|')
 };
