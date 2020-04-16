90e0e4838ee3509a04cfdd0edfbc5d347796bcd6
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-16 17:27:51 -0500

    begin deprecating v1

--- routes/api_deprecated.js
+++ routes/api_deprecated.js
@@ -7,4 +7,18 @@ router.get('/historical/:country', async (req, res) => res.send({ message: 'Depr
 
 router.get('/jhucsse', async (req, res) => res.send({ message: 'Deprecated, use /v2/jhucsse' }));
 
+router.get('/all', async (req, res) => res.send({ message: 'Deprecated, use /v2/all' }));
+
+router.get('/countries', async (req, res) => res.send({ message: 'Deprecated, use /v2/countries' }));
+
+router.get('/countries/:query', async (req, res) => res.send({ message: 'Deprecated, use /v2/countries' }));
+
+router.get('/states', async (req, res) => res.send({ message: 'Deprecated, use /v2/states' }));
+
+router.get('/yesterday', async (req, res) => res.send({ message: 'Deprecated, use /v2/countries?yesterday=true' }));
+
+router.get('/yesterday/all', async (req, res) => res.send({ message: 'Deprecated, use /v2/all?yesterday=true' }));
+
+router.get('/yesterday/:query', async (req, res) => res.send({ message: 'Deprecated, use /v2/countries?yesterday=true' }));
+
 module.exports = router;
