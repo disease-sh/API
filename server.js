673ca258540b0b9c64e406dd811e268678920580
Julian Pufler <puf17640@spengergasse.at>
2020-04-16 20:12:35 +0200

    added an endpoint to query cloudflare statistics data to show in the landing page ethan designed

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const app = express();
+const csrfProtection = require('csurf')({cookie: true});
 const logger = require('./utils/logger');
+const path = require('path');
+const fetch = require('node-fetch');
 const { redis, config, keys, scraper } = require('./routes/instances');
 
 const execAll = async () => {
@@ -20,15 +22,18 @@ const execAll = async () => {
 execAll();
 setInterval(execAll, config.interval);
 
-app.use(cors());
-app.get('/', async (req, res) => res.redirect('https://github.com/novelcovid/api'));
-
 app.get('/invite', async (req, res) =>
 	res.redirect('https://discordapp.com/oauth2/authorize?client_id=685268214435020809&scope=bot&permissions=537250880')
 );
 
 app.get('/support', async (req, res) => res.redirect('https://discord.gg/EvbMshU'));
 
+app.use(require('./routes/api_worldometers'));
+app.use(require('./routes/api_historical'));
+app.use(require('./routes/api_jhucsse'));
+app.use(require('./routes/api_deprecated'));
+
+app.use(require('cors')());
 app.use('/public', express.static('assets'));
 app.use('/docs',
 	swaggerUi.serve,
@@ -52,10 +57,32 @@ app.use('/docs',
 	})
 );
 
-app.use(require('./routes/api_worldometers'));
-app.use(require('./routes/api_historical'));
-app.use(require('./routes/api_jhucsse'));
-app.use(require('./routes/api_deprecated'));
+app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', 'ejs');
+app.use(require('cookie-parser')());
+
+// you have to change index.html to index.ejs and use ejs view engine (npm i ejs)
+// also add this to head of index.ejs so you can use it in the script.js:
+// <meta name="csrf-token" content="<%= csrfToken %>">
+app.get('/', csrfProtection, async (req, res) => res.render('index', {csrfToken: req.csrfToken()}));
+
+app.post('/private/cloudflare', require('body-parser').urlencoded({extended: true}), csrfProtection, async (req, res) => {
+	const zoneTag = 'b2070162162124e2d5414cee23dfe861';
+	const response = await fetch('https://api.cloudflare.com/client/v4/graphql', {
+		method: 'POST',
+		headers: {
+			"x-auth-key": config.cfApiKey,
+			"x-auth-email": "elitemythyt@gmail.com"
+		},
+		body: '"{"query":"{\n  viewer {\n    zones(filter: { zoneTag: '+ zoneTag +' }) {\n      httpRequests1dGroups(\n        orderBy: [date_ASC]\n        limit: 1000\n        filter: { date_gt: \"2019-07-15\" }\n      ) {\n        date: dimensions {\n          date\n        }\n        sum {\n          cachedBytes\n          bytes\n        }\n      }\n    }\n  }\n}","variables":{}}"'
+	});
+	if (response.status == 200) {
+		res.send(await response.json());
+	}else{
+		// return some kinda of error if you like
+		res.send(response);
+	}
+})
 
 app.listen(config.port, () =>
 	logger.info(`Your app is listening on port ${config.port}`)
