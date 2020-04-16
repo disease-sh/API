7a4bb304b4ba30e4aaccfda911d0ec3e9b35b9bf
Van Buskirk <vincent.vanbuskirk@nytimes.com>
2020-04-15 17:37:20 -0700

    lint

--- utils/nyt_cache.js
+++ utils/nyt_cache.js
@@ -15,7 +15,7 @@ exports.nytCounties = () => this.currentStatus.nytCounties;
 exports.nytStates = () => this.currentStatus.nytStates;
 exports.nytNationwide = () => this.currentStatus.nytNationwide;
 
-// Retrieves NYT data from Redis and stores it locally when 
+// Retrieves NYT data from Redis and stores it locally when data is updated
 exports.updateCache = async () => {
 	try {
 		const { redis, keys } = require('../routes/instances');
@@ -29,7 +29,7 @@ exports.updateCache = async () => {
 		this.currentStatus.nytNationwide = parsedNationData;
 		logger.info('NYT local cache updated');
 	} catch (err) {
-		logger.err('Local NYT cache update failed', err)
+		logger.err('Local NYT cache update failed', err);
 	}
 };
 
