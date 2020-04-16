090e70bbaeb4b56f7760d5d401d3a308176db98f
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-16 15:26:20 -0500

    checkpoint closing PR

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const swaggerUi = require('swagger-ui-express');
 const app = express();
 const csrfProtection = require('csurf')({ cookie: true });
@@ -64,24 +65,29 @@ app.get('/', csrfProtection, async (req, res) => res.render('index', { csrfToken
 
 app.post('/private/cloudflare', require('body-parser').urlencoded({ extended: true }), csrfProtection, async (req, res) => {
 	const zoneTag = 'b2070162162124e2d5414cee23dfe861';
-	const response = await require('axios')('https://api.cloudflare.com/client/v4/graphql', {
-		method: 'POST',
-		headers: {
-			'x-auth-key': config.cfApiKey,
-			'x-auth-email': 'elitemythyt@gmail.com'
-		},
-		body: `"{"query":"{\n  viewer {\n    zones(filter: { zoneTag: ${zoneTag} }) {\n      
-			httpRequests1dGroups(\n        orderBy: [date_ASC]\n        limit: 1000\n        
-			filter: { date_gt: "2019-07-15" }\n      ) {\n        date: dimensions {\n          
-			date\n        }\n        sum {\n          cachedBytes\n          bytes\n        }\n      }\n    }\n  }\n}","variables":{}}"`
-	});
+	let response;
+	try {
+		response = await axios.get('https://api.cloudflare.com/client/v4/graphql', {
+			method: 'POST',
+			headers: {
+				'x-auth-key': config.cfApiKey,
+				'x-auth-email': 'elitemythyt@gmail.com'
+			},
+			body: `"{"query":"{\n  viewer {\n    zones(filter: { zoneTag: ${zoneTag} }) {\n      
+				httpRequests1dGroups(\n        orderBy: [date_ASC]\n        limit: 1000\n        
+				filter: { date_gt: "2019-07-15" }\n      ) {\n        date: dimensions {\n          
+				date\n        }\n        sum {\n          cachedBytes\n          bytes\n        }\n      }\n    }\n  }\n}","variables":{}}"`
+		});
+	} catch (err) {
+		logger.err('Error: Requesting private/cloudflare failed', err);
+		res.send(err);
+		return;
+	}
 	if (response.status === 200) {
-		console.log('HIIIIII');
-		res.send(await response.json());
+		res.send(await response.data);
 	} else {
 		// return some kinda of error if you like
-		console.log('NOOOOOOO', response.status);
-		res.send(response);
+		res.send(response.error);
 	}
 });
 
