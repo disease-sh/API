8b93279c91c3bc8899d9c8f07f86a0284f8ec226
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-20 11:47:50 -0500

    start input

--- server.js
+++ server.js
@@ -71,32 +71,22 @@ app.use(require('cookie-parser')());
 app.get('/', csrfProtection, async (req, res) => res.render('index', { csrfToken: req.csrfToken() }));
 
 app.post('/private/cloudflare', require('body-parser').urlencoded({ extended: true }), csrfProtection, async (req, res) => {
-	const zoneTag = 'b2070162162124e2d5414cee23dfe861';
 	let response;
 	try {
-		response = await axios.get('https://api.cloudflare.com/client/v4/graphql', {
-			method: 'POST',
-			headers: {
-				'x-auth-key': config.cfApiKey,
-				'x-auth-email': 'elitemythyt@gmail.com'
-			},
-			body: `"{"query":"{\n  viewer {\n    zones(filter: { zoneTag: ${zoneTag} }) {\n      
-				httpRequests1dGroups(\n        orderBy: [date_ASC]\n        limit: 1000\n        
-				filter: { date_gt: "2019-07-15" }\n      ) {\n        date: dimensions {\n          
-				date\n        }\n        sum {\n          cachedBytes\n          bytes\n        }\n      }\n    }\n  }\n}","variables":{}}"`
-		});
+		response = await axios.get('https://disease.sh/v2/all');
 	} catch (err) {
 		logger.err('Error: Requesting private/cloudflare failed', err);
 		res.send(err);
 		return;
 	}
 	if (response.status === 200) {
-		res.send(await response.data);
+		res.send(response.data);
 	} else {
 		// return some kinda of error if you like
 		res.send(response.error);
 	}
 });
+
 app.use(require('./routes/api_worldometers'));
 app.use(require('./routes/api_historical'));
 app.use(require('./routes/api_jhucsse'));
