30b739dca2213d6ce4660288e4abf17d02ba941b
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-25 19:23:41 -0500

    fix linting errors

--- config/index.js
+++ config/index.js
@@ -1,5 +1,4 @@
 const keys = require('./config.keys.json');
-let port;
 let config;
 
 try {
@@ -7,7 +6,7 @@ try {
 } catch (err) {
 	config = require('./config.example.json');
 }
-port = process.env.SERVER_PORT || config.port || 3000;
+const port = process.env.SERVER_PORT || config.port || 3000;
 config.redis.host = process.env.REDIS_HOST ? process.env.REDIS_HOST : config.redis.host;
 config.redis.port = process.env.REDIS_PORT ? process.env.REDIS_PORT : config.redis.port;
 config.redis.password = process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : config.redis.password;
