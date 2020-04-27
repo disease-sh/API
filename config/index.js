08b3d7b3ca3cc1f88f586774aae62085c3c50cd3
Walter Corrales <corraleswalter@live.com>
2020-04-26 20:03:46 -0600

    ESLint Suggestions

--- config/index.js
+++ config/index.js
@@ -7,15 +7,20 @@ try {
 } catch (err) {
 	config = require('./config.example.json');
 }
-//SERVER PORT
+// SERVER PORT
 const port = process.env.SERVER_PORT || config.port || 3000;
-//REDIS CONFIGURATION
-config.redis.host = process.env.REDIS_HOST || config.redis.host || localhost;
+
+// REDIS CONFIGURATION
+config.redis.host = process.env.REDIS_HOST || config.redis.host || 'localhost';
 config.redis.port = process.env.REDIS_PORT || config.redis.port || 6379;
 config.redis.password = process.env.REDIS_PASSWORD || config.redis.password || '';
+
 // SCRAPER INTERVALS
-config.interval = process.env.INTERVAL || config.interval || 6e5; // DEFAULT 10 minutes
-config.nyt_interval = process.env.NYT_INTERVAL || config.nyt_interval || 864e5; // DEFAULT 24hrs
+// DEFAULT 10 minutes
+config.interval = process.env.INTERVAL || config.interval || 6e5;
+// DEFAULT 24hrs
+// eslint-disable-next-line camelcase
+config.nyt_interval = process.env.NYT_INTERVAL || config.nyt_interval || 864e5;
 
 module.exports = {
 	config,
