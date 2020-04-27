9b2aa728e7aa3fe09b29d242c3554ffa15881250
Walter Corrales <corraleswalter@live.com>
2020-04-26 19:47:42 -0600

    Interval support for .env file and config json adding default values

--- config/index.js
+++ config/index.js
@@ -7,10 +7,15 @@ try {
 } catch (err) {
 	config = require('./config.example.json');
 }
+//SERVER PORT
 const port = process.env.SERVER_PORT || config.port || 3000;
-config.redis.host = process.env.REDIS_HOST ? process.env.REDIS_HOST : config.redis.host;
-config.redis.port = process.env.REDIS_PORT ? process.env.REDIS_PORT : config.redis.port;
-config.redis.password = process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : config.redis.password;
+//REDIS CONFIGURATION
+config.redis.host = process.env.REDIS_HOST || config.redis.host || localhost;
+config.redis.port = process.env.REDIS_PORT || config.redis.port || 6379;
+config.redis.password = process.env.REDIS_PASSWORD || config.redis.password || '';
+// SCRAPER INTERVALS
+config.interval = process.env.INTERVAL || config.interval || 6e5; // DEFAULT 10 minutes
+config.nyt_interval = process.env.NYT_INTERVAL || config.nyt_interval || 864e5; // DEFAULT 24hrs
 
 module.exports = {
 	config,
