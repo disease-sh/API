d898fa26cdb71ca1750c816681267c3324d4a710
Julian Pufler <puf17640@spengergasse.at>
2020-04-30 19:17:09 +0200

    now sending historical data with homepage

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const csrfProtection = require('csurf')({ cookie: true });
 const bodyParser = require('body-parser');
 const logger = require('./utils/logger');
 const path = require('path');
-const { config, port } = require('./routes/instances');
+const { config, port, redis, scraper, keys } = require('./routes/instances');
 const { updateCache } = require('./utils/nyt_cache');
 
 if (config.sentry_key) Sentry.init({ dsn: config.sentry_key });
@@ -40,7 +40,10 @@ app.set('views', path.join(__dirname, '/public'));
 app.set('view engine', 'ejs');
 app.use(require('cookie-parser')());
 
-app.get('/', csrfProtection, async (req, res) => res.render('index', { csrfToken: req.csrfToken() }));
+app.get('/', csrfProtection, async (req, res) => res.render('index', { 
+		csrfToken: req.csrfToken(), 
+		chartData: await scraper.historical.getHistoricalAllDataV2(JSON.parse(await redis.get(keys.historical_v2)), 'all') 
+	}));
 
 app.get('/invite', async (req, res) =>
 	res.redirect('https://discordapp.com/oauth2/authorize?client_id=685268214435020809&scope=bot&permissions=537250880'));
