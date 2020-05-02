326e0eee2390ffdad0cebccc090a9113a4923cf9
Julian Pufler <puf17640@spengergasse.at>
2020-05-02 13:33:57 +0200

    reworked the apple endpoints a bit

--- routes/api_apple.js
+++ routes/api_apple.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 const countryUtils = require('../utils/country_utils');
+const { splitQuery } = require('../utils/string_utils');
 const { appleData } = require('../utils/apple_cache');
 
 router.get('/v2/apple/countries/:country?', async (req, res) => {
-	const { country: queryCountry } = req.params;
+	let { country: countryName } = req.params;
 	const data = appleData();
-	if (queryCountry) {
-		const countryName = queryCountry.trim();
-		const standardizedCountryName = countryUtils.getCountryData(countryName).country || countryName;
+	if (countryName) {
+		const standardizedCountryName = countryUtils.getCountryData(countryName.trim()).country || countryName.trim();
 		if (data[standardizedCountryName] && data[standardizedCountryName].subregions) {
 			res.send({ country: standardizedCountryName, subregions: data[standardizedCountryName].subregions });
 		} else {
@@ -20,26 +20,21 @@ router.get('/v2/apple/countries/:country?', async (req, res) => {
 });
 
 router.get('/v2/apple/countries/:country/:subregions', async (req, res) => {
-	const { country: queryCountry, subregions: querySubregions } = req.params;
-	const data = appleData();
-	if (queryCountry && querySubregions) {
-		const countryName = queryCountry.trim();
-		const standardizedCountryName = countryUtils.getCountryData(countryName).country || countryName;
-		const countryData = data[standardizedCountryName];
-		const subregions = querySubregions.trim().split(/,[\s+?]/).map((subregion) => subregion.toLowerCase());
+	const { country: countryName, subregions: querySubregions } = req.params;
+	if (countryName && querySubregions) {
+		const standardizedCountryName = countryUtils.getCountryData(countryName.trim()).country || countryName.trim();
+		const countryData = appleData()[standardizedCountryName];
+		const subregions = splitQuery(querySubregions).map((subregion) => subregion.trim().toLowerCase());
 		const subregiondata = subregions.map((subregion) => {
-			const notFoundMessage = `Subregion '${subregion}' not found for '${standardizedCountryName}'`;
-			if (countryData && countryData.data) {
-				const subregionData = countryData.data.filter((element) => element.subregion_and_city.toLowerCase() === subregion);
-				return {
-					subregion: subregion,
-					data: subregionData.length > 0 ? subregionData : { message: notFoundMessage }
-				};
-			} else {
-				return { subregion: subregion, message: notFoundMessage };
+			const data = { subregion, message: `Subregion '${subregion}' not found for '${standardizedCountryName}'` };
+			const subdata = countryData && countryData.data && countryData.data.filter((element) => element.subregion_and_city.toLowerCase() === subregion);
+			if (subdata && subdata.length > 0) {
+				delete data.message;
+				data.data = subdata;
 			}
+			return data;
 		});
-		res.send(subregiondata);
+		res.send(subregiondata.length > 1 ? subregiondata : subregiondata[0]);
 	} else {
 		res.status(404).send({ message: 'Subregion not found or no data found for county' });
 	}
--- tests/mochaSetup.spec.js
+++ tests/mochaSetup.spec.js
@@ -1,12 +1,13 @@
 const { scraper: { executeScraper, executeScraperNYTData, excecuteScraperAppleData }, redis } = require('../routes/instances');
+const logger = require('../utils/logger');
 
 // eslint-disable-next-line
 before(async () => {
 	await redis.flushall();
-	console.log('Finished flushing all data from redis.');
+	logger.info('Finished flushing all data from redis.');
 
 	await executeScraper();
 	await executeScraperNYTData();
 	await excecuteScraperAppleData();
-	console.log('Scraping finished.');
+	logger.info('Scraping finished.');
 });
