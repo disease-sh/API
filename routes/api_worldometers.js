0e97be1fb9f70390299f58066e18f4dcf7aed423
ebwinters@comcast.net <ebwinters@comcast.net>
2020-05-04 23:34:26 -0500

    refactor the api worldometer and wordtobool

--- routes/api_worldometers.js
+++ routes/api_worldometers.js
@@ -22,22 +22,20 @@ const fixApostrophe = (country) => {
 	country.country = country.country.replace(/"/g, '\'');
 	return country;
 };
-// hotifx for wordToBoolean
-router.get('/v2/all', async (req, res) => res.send(await getAllData(wordToBoolean(`${req.query.yesterday}`) ? keys.yesterday_countries : keys.countries)));
 
-// hotifx for wordToBoolean
+router.get('/v2/all', async (req, res) => res.send(await getAllData(wordToBoolean(req.query.yesterday) ? keys.yesterday_countries : keys.countries)));
+
 router.get('/v2/countries', async (req, res) => {
 	const { sort, yesterday } = req.query;
-	const countries = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_countries : keys.countries))
+	const countries = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_countries : keys.countries))
 		.filter(country => country.country.toLowerCase() !== 'world').map(fixApostrophe);
 	res.send(sort ? countries.sort((a, b) => a[sort] > b[sort] ? -1 : 1) : countries);
 });
 
-// hotifx for wordToBoolean
 router.get('/v2/countries/:query', async (req, res) => {
 	const { yesterday, strict } = req.query;
 	const { query } = req.params;
-	let countries = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_countries : keys.countries))
+	let countries = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_countries : keys.countries))
 		.filter(country => country.country.toLowerCase() !== 'world').map(fixApostrophe);
 	countries = splitQuery(query)
 		.map(country => countryUtils.getWorldometersData(countries, country, strict !== 'false'))
@@ -46,41 +44,37 @@ router.get('/v2/countries/:query', async (req, res) => {
 	else res.status(404).send({ message: 'Country not found or doesn\'t have any cases' });
 });
 
-// hotifx for wordToBoolean
 router.get('/v2/continents', async (req, res) => {
 	const { sort, yesterday } = req.query;
-	const countries = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_countries : keys.countries));
-	const continents = await Promise.all(JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_continents : keys.continents))
+	const countries = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_countries : keys.countries));
+	const continents = await Promise.all(JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_continents : keys.continents))
 		.map(async continent => ({ ...continent, countries: countryUtils.getCountriesFromContinent(continent.continent, countries) })));
 	res.send(sort ? continents.sort((a, b) => a[sort] > b[sort] ? -1 : 1) : continents);
 });
 
-// hotifx for wordToBoolean
 router.get('/v2/continents/:query', async (req, res) => {
 	const { yesterday, strict } = req.query;
 	const { query } = req.params;
-	const continents = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_continents : keys.continents));
+	const continents = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_continents : keys.continents));
 	const continent = countryUtils.getWorldometersData(continents, query, strict !== 'false', true);
 	if (continent) {
-		continent.countries = countryUtils.getCountriesFromContinent(continent.continent, JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_countries : keys.countries)));
+		continent.countries = countryUtils.getCountriesFromContinent(continent.continent, JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_countries : keys.countries)));
 		res.send(continent);
 	} else {
 		res.status(404).send({ message: 'Continent not found or doesn\'t have any cases' });
 	}
 });
 
-// hotifx for wordToBoolean
 router.get('/v2/states', async (req, res) => {
 	const { sort, yesterday } = req.query;
-	const states = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_states : keys.states)).splice(1);
+	const states = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_states : keys.states)).splice(1);
 	res.send(sort ? states.sort((a, b) => a[sort] > b[sort] ? -1 : 1) : states);
 });
 
-// hotifx for wordToBoolean
 router.get('/v2/states/:query', async (req, res) => {
 	const { yesterday } = req.query;
 	const { query } = req.params;
-	const states = JSON.parse(await redis.get(wordToBoolean(`${yesterday}`) ? keys.yesterday_states : keys.states)).splice(1);
+	const states = JSON.parse(await redis.get(wordToBoolean(yesterday) ? keys.yesterday_states : keys.states)).splice(1);
 	const stateData = splitQuery(query)
 		.map(state => states.find(state2 => state.toLowerCase() === state2.state.toLowerCase()))
 		.filter(value => value);
