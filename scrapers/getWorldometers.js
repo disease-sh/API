59bfa1a942d01bc820b18e0bd0aa875cdef83217
pigeontoe <electriclady1@gmail.com>
2020-05-08 13:36:55 -0400

    use isNaN to cover all missing data cases

--- scrapers/getWorldometers.js
+++ scrapers/getWorldometers.js
@@ -47,7 +47,7 @@ const mapRows = (_, row) => {
 				break;
 
 			default:
-				country[columns[index - 1]] = cell.text() === '' ? null : (parseInt(cell.text().replace(/(\n|,)/g, '')) || 0);
+				country[columns[index - 1]] = isNaN(parseInt(cell.text().replace(/(\n|,)/g, ''))) ? null : parseInt(cell.text().replace(/(\n|,)/g, ''));
 		}
 	});
 	return country;
