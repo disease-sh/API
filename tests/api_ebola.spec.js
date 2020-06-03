d1b2814bb2016da791911a490146b7e3a8e7494d
ebwinters@comcast.net <ebwinters@comcast.net>
2020-06-03 11:08:04 -0400

    fix test

--- tests/api_ebola.spec.js
+++ tests/api_ebola.spec.js
@@ -6,25 +6,25 @@ const { testBasicProperties } = require('./testingFunctions');
 chai.use(chaiHttp);
 
 describe('TESTING /v2/ebola general', () => {
-    it('/v2/ebola correct properties', (done) => {
+	it('/v2/ebola correct properties', (done) => {
 		chai.request(app)
 			.get('/v2/ebola')
 			.end((err, res) => {
 				testBasicProperties(err, res, 200, 'object');
-                res.body.should.have.property('updated');
-                res.body.should.have.property('source');
-                res.body.should.have.property('provinces');
+				res.body.should.have.property('updated');
+				res.body.should.have.property('source');
+				res.body.should.have.property('provinces');
 				done();
 			});
-    });
-    
-    it('/v2/ebola provinces filled', (done) => {
+	});
+	
+	it('/v2/ebola provinces filled', (done) => {
 		chai.request(app)
 			.get('/v2/ebola')
 			.end((err, res) => {
 				testBasicProperties(err, res, 200, 'object');
-                res.body.should.have.property('provinces');
-                res.body.provinces.length.should.be.at.least(1);
+				res.body.should.have.property('provinces');
+				res.body.provinces.length.should.be.at.least(1);
 				done();
 			});
 	});
@@ -34,8 +34,8 @@ describe('TESTING /v2/ebola general', () => {
 			.get('/v2/ebola')
 			.end((err, res) => {
 				testBasicProperties(err, res, 200, 'object');
-                res.body.should.have.property('provinces');
-                res.body.provinces.forEach((obj) => {
+				res.body.should.have.property('provinces');
+				res.body.provinces.forEach((element) => {
 					element.cases.should.be.at.least(0);
 					element.probableCases.should.be.at.least(0);
 					element.deaths.should.be.at.least(0);
