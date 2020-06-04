a3c6a84d1beee63f8004cbe652f1d5ef0b03bd6b
ebwinters@comcast.net <ebwinters@comcast.net>
2020-06-04 14:17:03 -0400

    add v3 routes while still supporting v2 routes

---
+++ routes/v3/covid19/apiDeprecated.js
@@ -0,0 +1,84 @@
+// eslint-disable-next-line new-cap
+const router = require('express').Router();
+
+router.get('/historical', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/historical instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/historical/:country', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/historical instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/jhucsse', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/jhucsse instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/all', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/all instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/countries', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/countries instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/countries/:query', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/countries instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/states', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/states instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/yesterday', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/countries?yesterday=true instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/yesterday/all', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/all?yesterday=true instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+router.get('/yesterday/:query', async (req, res) => res
+	.header('Warning', '299 - "This endpoint is deprecated. Check out the docs for details."')
+	.status(410)
+	.send({
+		message: 'This endpoint has been deprecated. Use /v2/countries?yesterday=true instead.',
+		docs: `${req.protocol}://${req.get('host')}/docs`
+	}));
+
+module.exports = router;
---
+++ routes/v3/covid19/apiGov.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line new-cap
+const router = require('express').Router();
+const countryUtils = require('../../../utils/countryUtils');
+const { wordToBoolean } = require('../../../utils/stringUtils');
+const { redis, keys } = require('../../instances');
+
+router.get('/v3/covid19/gov/:country?', async (req, res) => {
+	const { allowNull } = req.query;
+	const { country: countryName } = req.params;
+	const data = JSON.parse(await redis.get(keys.gov_countries));
+	if (countryName) {
+		const standardizedCountryName = countryUtils.getCountryData(countryName.trim()).country || countryName.trim();
+		if (data[standardizedCountryName]) {
+			res.send(!wordToBoolean(allowNull) ? countryUtils.transformNull(data[standardizedCountryName]) : data[standardizedCountryName]);
+		} else {
+			res.status(404).send({ message: `Country '${standardizedCountryName}' not found or no data found for county` });
+		}
+	} else {
+		res.send(Object.keys(data));
+	}
+});
+
+module.exports = router;
