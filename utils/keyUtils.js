31f88e56bcba3e1ca54cc142aef84fbd9a54a131
jsebastianms1 <jsebastianms1@gmail.com>
2020-06-17 11:57:15 -0500

    param order

--- utils/keyUtils.js
+++ utils/keyUtils.js
@@ -2,8 +2,8 @@ const keys = require('./keysTranslation');
 
 /**
  * Returns renamed keys based on country
- * @param {string} country iso2 for a country
  * @param {Array} data country array of places
+ * @param {string} country iso2 for a country
  * @returns {Array} Country data for each place with the new keys
  */
 const renameKeys = (data, country) => {
