3e3f9197b20f35e99ca5020bd9616540b1377855
ebwinters@comcast.net <ebwinters@comcast.net>
2020-06-17 14:21:47 -0400

    update india/colombia v3 tests

--- tests/v2/api_gov.spec.js
+++ tests/v2/api_gov.spec.js
@@ -346,18 +346,18 @@ describe('TESTING /v2/gov/new zealand', () => {
 			});
   });
 
-  describe('TESTING /v2/gov/colombia', () => {
+describe('TESTING /v2/gov/colombia', () => {
     it('/v2/gov/colombia correct fields set', (done) => {
-      chai.request(app)
-        .get('/v2/gov/colombia')
-        .end((err, res) => {
-          testBasicProperties(err, res, 200, 'object');
-          res.body.should.have.property('updated');
-          res.body.should.have.property('departments');
-          res.body.should.have.property('cities');
-          res.body.departments.length.should.be.at.least(32);
-          done();
-        });
-    });
-  });
+    	chai.request(app)
+			.get('/v2/gov/colombia')
+			.end((err, res) => {
+				testBasicProperties(err, res, 200, 'object');
+				res.body.should.have.property('updated');
+				res.body.should.have.property('departments');
+				res.body.should.have.property('cities');
+				res.body.departments.length.should.be.at.least(32);
+				done();
+        	});
+    	});
+  	});
 });
