2e1ff7afe9bfcf6c9b796c2400d947d7718aab50
Meir Roth <12494197+meirroth@users.noreply.github.com>
2020-07-06 02:44:48 +0300

    Add / modify endpoints
    Removed sickPerLocation request and replaced it with more detailed info in the timeline.
    Added active cases by condition.
    Added back the city lock down status.
    Added isolated doctors and nurses.
    Added hospital stats.

--- scrapers/covid-19/govScrapers/getIsrael.js
+++ scrapers/covid-19/govScrapers/getIsrael.js
@@ -4,13 +4,14 @@ const logger = require('../../../utils/logger');
 const params = {
 	requests: [
 		{ id: 0, queryName: 'lastUpdate', single: true, parameters: {} },
-		{ id: 4, queryName: 'sickPerLocation', single: false, parameters: {} },
 		{ id: 5, queryName: 'patientsPerDate', single: false, parameters: {} },
 		{ id: 6, queryName: 'deadPatientsPerDate', single: false, parameters: {} },
 		{ id: 7, queryName: 'recoveredPerDay', single: false, parameters: {} },
 		{ id: 8, queryName: 'testResultsPerDate', single: false, parameters: {} },
 		{ id: 12, queryName: 'infectedByAgeAndGenderPublic', single: false, parameters: { ageSections: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] } },
-		{ id: 15, queryName: 'contagionDataPerCityPublic', single: false, parameters: {} }
+		{ id: 13, queryName: 'isolatedDoctorsAndNurses', single: false, parameters: {} },
+		{ id: 15, queryName: 'contagionDataPerCityPublic', single: false, parameters: {} },
+		{ id: 16, queryName: 'hospitalStatus', single: false, parameters: {} }
 	]
 };
 
@@ -21,30 +22,43 @@ const params = {
  */
 const parseData = (data) => {
 	const dateRegex = /\d+-\d+-\d+/g;
-	const patientsPerDay = data[2].data;
-	const deadPatientsPerDay = data[3].data;
-	const recoveredPerDay = data[4].data.splice(20, data[4].data.length - 1);
-	const testsPerDay = data[5].data.splice(30, data[5].data.length - 1);
+	const patientsPerDay = data[1].data;
+	const deadPatientsPerDay = data[2].data;
+	const recoveredPerDay = data[3].data.splice(20, data[3].data.length - 1);
+	const testsPerDay = data[4].data.splice(30, data[4].data.length - 1);
 	const timeline = patientsPerDay.map((elem, index) => ({
 		date: elem.date.match(dateRegex)[0],
 		newHospitalized: elem.new_hospitalized,
-		totalhospitalized: elem.Counthospitalized,
+		hospitalPatients:  elem.Counthospitalized,
+		homePationts:  elem.patients_home,
+		hotelPationts: elem.patients_hotel,
 		totalBeds: elem.total_beds,
 		StandardOccupancy: elem.StandardOccupancy,
 		newDeaths: deadPatientsPerDay[index].amount,
 		newlyRecovered: recoveredPerDay[index].amount,
 		newTestsTaken: testsPerDay[index].amount,
-		newPositiveTests: testsPerDay[index].positiveAmount
+		newPositiveTests: testsPerDay[index].positiveAmount,
+		activeEasyStatus: elem.CountEasyStatus,
+		activeMediumStatus: elem.CountMediumStatus,
+		activeCriticalStatus: elem.CountHardStatus,
+		onVentilators: elem.CountBreath
 	}));
 	return {
 		updated: new Date(data[0].data.lastUpdate).valueOf(),
 		data: {
-			sickLocated: { home: data[1].data[0].amount, hospital: data[1].data[1].amount },
-			sickByAge: data[6].data.map((entry) => {
+			sickByAge: data[5].data.map((entry) => {
 				const { section, male, female } = entry;
 				return { section: `ages ${section} - ${section + 10}`, male, female };
 			}),
-			cityData: data[7].data.map((entry) => delete entry.status && entry),
+			missingStaff: {
+				verifiedDoctors: data[6].data[0].Verified_Doctors,
+				verifiedNurses: data[6].data[0].Verified_Nurses,
+				isolatedDoctors: data[6].data[0].isolated_Doctors,
+				isolatedNurses: data[6].data[0].isolated_Nurses,
+				isolatedOtherSector: data[6].data[0].isolated_Other_Sector
+			},
+			hospitalData: data[8].data,
+			cityData: data[7].data,
 			timeline
 		}
 	};
