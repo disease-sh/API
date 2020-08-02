3926b7eabea8733604cc686f48aca10f7f68ba9e
Aditya Kumar <adityak007mr@gmail.com>
2020-08-02 07:20:29 +0530

    fixed linting tests

--- routes/v3/covid-19/apiVaccineStats.js
+++ routes/v3/covid-19/apiVaccineStats.js
@@ -7,14 +7,14 @@ const { response } = require('express');
 router.get('/v3/covid-19/vaccineStats', async (req, res) => {
 	const data = JSON.parse(await redis.get(keys.vaccine));
 	if (data) {
-		const temp = _.countBy(data.data, "trialPhase");
-		let struct = Object.keys(temp).map((key) => ({
+		const temp = _.countBy(data.data, 'trialPhase');
+		const struct = Object.keys(temp).map((key) => ({
 			phase: key,
-			candidates: temp[key] 
+			candidates: temp[key]
 		}));
 		var responseData = {};
-		responseData["totalCandidates"] = _.size(data.data);
-		responseData["phases"] = struct;
+		responseData.totalCandidates = _.size(data.data);
+		responseData.phases = struct;
 		res.send(responseData);
 	} else {
 		res.status(404).send({ message: `Error fetching vaccine stats` });
