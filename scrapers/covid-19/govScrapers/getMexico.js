171a1a15b6a7c9c1a267453d89b940d96e093441
collin-st <collin.street.dr@gmail.com>
2020-08-03 20:55:50 -0700

    Mexico gov scraper

--- scrapers/covid-19/govScrapers/getMexico.js
+++ scrapers/covid-19/govScrapers/getMexico.js
@@ -1,71 +1,97 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 const logger = require('../../../utils/logger');
+const querystring = require('querystring');
 
-/* eslint quote-props: ["error", "consistent"] */
-const mapStateToCve = {
-	'nacional': '000',
-	'aguascalientes': '01',
-	'baja california': '02',
-	'baja california Sur': '03',
-	'campeche': '04',
-	'coahuila': '05',
-	'colima': '06',
-	'chiapas': '07',
-	'chihuahua': '08',
-	'ciudad de mexico': '09',
-	'durango': '10',
-	'estado de mexico': '15',
-	'guanajuato': '11',
-	'guerrero': '12',
-	'hidalgo': '13',
-	'jalisco': '14',
-	'michoacan': '16',
-	'morelos': '17',
-	'nayarit': '18',
-	'nuevo leon': '19',
-	'oaxaca': '20',
-	'puebla': '21',
-	'queretaro': '22',
-	'quintana roo': '23',
-	'san luis potosi': '24',
-	'sinaloa': '25',
-	'sonora': '26',
-	'tabasco': '27',
-	'tamaulipas': '28',
-	'tlaxcala': '29',
-	'veracruz': '30',
-	'yucatan': '31',
-	'zacatecas': '32'
+const stateIDs = [
+	{ state: 'aguascalientes', id: '01' },
+	{ state: 'baja california', id: '02' },
+	{ state: 'baja california sur', id: '03' },
+	{ state: 'campeche', id: '04' },
+	{ state: 'coahuila', id: '05' },
+	{ state: 'colima', id: '06' },
+	{ state: 'chiapas', id: '07' },
+	{ state: 'chihuahua', id: '08' },
+	{ state: 'ciudad de mexico', id: '09' },
+	{ state: 'durango', id: '10' },
+	{ state: 'guanajuato', id: '11' },
+	{ state: 'guerrero', id: '12' },
+	{ state: 'hidalgo', id: '13' },
+	{ state: 'jalisco', id: '14' },
+	{ state: 'estado de mexico', id: '15' },
+	{ state: 'michoacan', id: '16' },
+	{ state: 'morelos', id: '17' },
+	{ state: 'nayarit', id: '18' },
+	{ state: 'nuevo leon', id: '19' },
+	{ state: 'oaxaca', id: '20' },
+	{ state: 'puebla', id: '21' },
+	{ state: 'queretaro', id: '22' },
+	{ state: 'quintana roo', id: '23' },
+	{ state: 'san luis potosi', id: '24' },
+	{ state: 'sinaloa', id: '25' },
+	{ state: 'sonora', id: '26' },
+	{ state: 'tabasco', id: '27' },
+	{ state: 'tamaulipas', id: '28' },
+	{ state: 'tlaxcala', id: '29' },
+	{ state: 'veracruz', id: '30' },
+	{ state: 'yucatan', id: '31' },
+	{ state: 'zacatecas', id: '32' }
+];
+
+const formData = {
+	nationalCases: querystring.stringify({ cve: '000', nom: 'nacional', sPatType: 'Confirmados' }),
+	nationalDeaths: querystring.stringify({ cve: '000', nom: 'nacional', sPatType: 'Defunciones' }),
+	stateCases: querystring.stringify({ cve: '', nom: '', sPatType: 'Confirmados' }),
+	stateDeaths: querystring.stringify({ cve: '', nom: '', sPatType: 'Defunciones' })
 };
 
-// const mapRows = (_, row) => {
-// 	const province = { updated: Date.now() };
-// 	cheerio(row).children('td').each((index, cell) => {
-// 		cell = cheerio.load(cell);
-// 		switch (index) {
-// 			case 0: {
-// 				province[columns[index]] = cell.text() === 'Canada' ? 'Total' : cell.text();
-// 				break;
-// 			}
-// 			default: {
-// 				province[columns[index]] = parseInt(cell.text().replace(/,/g, '')) || null;
-// 			}
-// 		}
-// 	});
-// 	return province;
-// };
+const filterByDate = (data) => {
+	const date = new Date();
+	date.setDate(date.getDate() - 1);
+	return data.filter(item => item.date === `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`);
+};
+
+const getNational = (res) => {
+	const recovered = parseInt(res.substring(res.indexOf('gsRecDIV'), res.indexOf('gsRecDIV') + 35).split('(')[1].split(')')[0]);
+	const data = filterByDate(JSON.parse(res.substring(res.lastIndexOf('['), res.lastIndexOf(']') + 1)))[0];
+	return { recovered, data };
+};
+
+const getState = (res) => {
+	const stateValues = {};
+	stateIDs.forEach(state => {
+		const target = `Total['${state.id}']`;
+		const value = parseInt(res.substring(res.indexOf(target), res.indexOf(target) + 18).split(';')[0].split('=')[1]);
+		stateValues[state.state] = value;
+	});
+	return stateValues;
+};
 
 const mexicoData = async () => {
 	try {
-		const html = cheerio.load((await axios.get('https://coronavirus.gob.mx/datos/Overview/overView.php')).data);
-		return html(`table#dataTable`).children('tbody:first-of-type').children('tr');
+		const nationalCases = getNational((await axios.post('https://coronavirus.gob.mx/datos/Overview/info/getInfo.php', formData.nationalCases)).data);
+		const nationalDeaths = getNational((await axios.post('https://coronavirus.gob.mx/datos/Overview/info/getInfo.php', formData.nationalDeaths)).data);
+		const stateCases = getState((await axios.post('https://coronavirus.gob.mx/datos/Overview/info/getInfo.php', formData.stateCases)).data);
+		const stateDeaths = getState((await axios.post('https://coronavirus.gob.mx/datos/Overview/info/getInfo.php', formData.stateDeaths)).data);
+
+		const states = stateIDs.map(state => ({ [state.state]: { cases: stateCases[state.state], deaths: stateDeaths[state.state] } }));
+
+		return {
+			updated: Date.now(),
+			national: {
+				newCases: nationalCases.data.total,
+				newDeaths: nationalDeaths.data.total,
+				cases: nationalCases.data.acumulado,
+				deaths: nationalDeaths.data.acumulado,
+				recovered: nationalCases.recovered
+
+			},
+			states,
+			source: 'https://coronavirus.gob.mx/datos/'
+		};
 	} catch (err) {
 		logger.err('Error: Requesting Mexico Gov Data failed!', err);
 		return null;
 	}
 };
-
-mexicoData();
 
 module.exports = mexicoData;
