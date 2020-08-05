be515dba88fc78ecfd1a0d2d0f024e8bd90acb27
collin-st <collin.street.dr@gmail.com>
2020-08-04 17:36:21 -0700

    fix to accommodate UK api change

--- scrapers/covid-19/govScrapers/getUK.js
+++ scrapers/covid-19/govScrapers/getUK.js
@@ -3,7 +3,7 @@ const logger = require('../../../utils/logger');
 
 const ukData = async () => {
 	try {
-		var data = { };
+		var data = {};
 		const structure = {
 			date: 'date',
 			todayTests: 'newTestsByPublishDate',
@@ -18,7 +18,7 @@ const ukData = async () => {
 			newDeaths: 'newDeathsByPublishDate',
 			deaths: 'cumDeathsByPublishDate'
 		};
-		const response = (await axios.get(`https://api.coronavirus-staging.data.gov.uk/v1/data?filters=areaName=United%20Kingdom;areaType=overview&structure=${JSON.stringify(structure)}`)).data;
+		const response = (await axios.get(`https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=United%20Kingdom;areaType=overview&structure=${JSON.stringify(structure)}`)).data;
 		for (const row of response.data.slice(0, response.length - 60)) {
 			data[row.date] = row;
 			delete data[row.date].date;
