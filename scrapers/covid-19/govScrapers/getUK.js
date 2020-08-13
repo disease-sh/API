212dafcc13a2bb4687f1299c27e952b9d95d48f5
ebwinters@comcast.net <ebwinters@comcast.net>
2020-08-13 18:14:04 -0400

    Fix UK gov endpoint

--- scrapers/covid-19/govScrapers/getUK.js
+++ scrapers/covid-19/govScrapers/getUK.js
@@ -14,10 +14,9 @@ const ukData = async () => {
 			hospitalized: 'hospitalCases',
 			usedVentilationBeds: 'covidOccupiedMVBeds',
 			newAdmissions: 'newAdmissions',
-			admissions: 'cumAdmissions',
-			newDeaths: 'newDeathsByPublishDate',
-			deaths: 'cumDeathsByPublishDate'
+			admissions: 'cumAdmissions'
 		};
+		console.log(`https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=United%20Kingdom;areaType=overview&structure=${JSON.stringify(structure)}`);
 		const response = (await axios.get(`https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=United%20Kingdom;areaType=overview&structure=${JSON.stringify(structure)}`)).data;
 		for (const row of response.data.slice(0, response.length - 60)) {
 			data[row.date] = row;
