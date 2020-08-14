89ea38d5b0b9241447ee16bae8c028a22a001ed7
rohitjethoe <rohitchai@icloud.com>
2020-08-14 16:37:48 +0200

    Removed 2 modules from v2 apiApple.

--- routes/v2/apiApple.js
+++ routes/v2/apiApple.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 const nameUtils = require('../../utils/nameUtils');
-const { splitQuery } = require('../../utils/stringUtils');
-const { appleData } = require('../../utils/cache');
 
 router.get('/v2/apple/countries/:country?', async (req, res) => {
 	const { country: countryName } = req.params;
