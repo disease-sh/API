d96415d093da17f0194fda631d44fd2e59f2cd48
Julian Pufler <puf17640@spengergasse.at>
2020-08-24 07:28:14 +0200

    changes requested by ethan

---
+++ checkOutOfDate.js
@@ -0,0 +1,78 @@
+const axios = require('axios'),
+	{ config } = require('./routes/instances'),
+	logger = require('./utils/logger');
+
+const now = new Date(),
+	rangeExtension = 1.5,
+	jhuRange = 1000 * 60 * 60 * 24 * rangeExtension;
+
+const endpoints = {
+	'covid-19': {
+		all: (data) => data.updated && now - new Date(data.updated) > config.worldometersInterval * rangeExtension,
+		countries: (data) => data[0].updated && now - new Date(data[0].updated) > config.worldometersInterval * rangeExtension,
+		continents: (data) => data[0].updated && now - new Date(data[0].updated) > config.worldometersInterval * rangeExtension,
+		states: (data) => data[0].updated && now - new Date(data[0].updated) > config.worldometersInterval * rangeExtension,
+		// 1.5 days
+		jhucsse: (data) => data[0].updated && now - new Date(data[0].updatedAt) > jhuRange,
+		// 1.5 days
+		'jhucsse/counties': (data) => data[0].updated && now - new Date(data[0].updatedAt) > jhuRange,
+		'gov/italy': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/south%20africa': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/switzerland': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/germany': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/nigeria': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/canada': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/india': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/israel': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/new%20zealand': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/austria': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/vietnam': (data) => data[0].updated && now - new Date(data[0].updated) > config.govInterval * rangeExtension,
+		'gov/mexico': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension,
+		'gov/colombia': (data) => data.updated && now - new Date(data.updated) > config.govInterval * rangeExtension
+	},
+	influenza: []
+};
+
+const sendWebhook = async (data) => {
+	try {
+		await axios.post(process.env.UPDATE_WEBHOOK, data);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const checkOutOfDate = async () => {
+	for (const disease of Object.keys(endpoints)) {
+		for (const [endpoint, checker] of Object.entries(endpoints[disease])) {
+			logger.info(`checking ${endpoint}`);
+			try {
+				const res = await axios.get(`https://disease.sh/v3/${disease}/${endpoint}`);
+				// eslint-disable-next-line no-unused-expressions
+				if (checker(res.data)) {
+					logger.info('OUT OF DATE - sending Webhook message');
+					await sendWebhook({
+						embeds: [
+							{
+								title: `${disease}/${endpoint} - Out of Date`,
+								url: `https://disease.sh/v3/${disease}/${endpoint}`
+							}
+						]
+					});
+				}
+			} catch (err) {
+				logger.info('ERROR - sending Webhook message');
+				await sendWebhook({
+					embeds: [
+						{
+							title: `${disease}/${endpoint} - Error`,
+							url: `https://disease.sh/v3/${disease}/${endpoint}`,
+							description: err.message || 'No message'
+						}
+					]
+				});
+			}
+		}
+	}
+};
+
+module.exports = checkOutOfDate;
