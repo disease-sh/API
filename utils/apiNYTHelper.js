9a33d2aa490155e7cb2c91f4714274c2b5457fbe
collin-st <collin.street.dr@gmail.com>
2020-08-31 23:22:35 -0700

    add lastdays to nyt/usa; clean up controller functions

--- utils/apiNYTHelper.js
+++ utils/apiNYTHelper.js
@@ -23,27 +23,35 @@ const nytCounties = async (lastdays = 30, key, redis) => {
 };
 
 const nytStates = async (lastdays = 30, key, redis) => {
-	const nytdata = await fetchNYTCache(key, redis);
+	const nytdata = await fetchNYTCache(key, redis, lastdays);
 	if (lastdays === 'all') {
 		return nytdata;
 	} else {
 		const priorDate = await calculatePriorDate(key, lastdays, 'latest');
-		return nytdata.filter((data) => data.date >= priorDate);
+		return await redis.hexists(key, lastdays)
+			? nytdata
+			: nytdata.filter((data) => data.date >= priorDate)
 	}
 };
 
-const nytNationwide = async (key, redis) => await fetchNYTCache(key, redis);
+const nytNationwide = async (lastdays = 'data', key, redis) => {
+	const nytdata = await fetchNYTCache(key, redis, lastdays);
+	if (lastdays === 'all') {
+		return nytdata;
+	} else {
+		const priorDate = await calculatePriorDate(key, lastdays, 'latest');
+		return await redis.hexists(key, lastdays)
+			? nytdata
+			: nytdata.filter((data) => data.date >= priorDate)
+	}
+};
 
 const fetchNYTCache = async (key, redis, lastdays) => {
 	let parsedData = '';
 	try {
-		if (key === 'covidapi:nyt_counties') {
-			(await redis.hexists(key, lastdays))
-				? parsedData = JSON.parse(await redis.hget(key, lastdays))
-				: parsedData = JSON.parse(await redis.hget(key, 'data'));
-		} else {
-			parsedData = JSON.parse(await redis.hget(key, 'data'));
-		}
+		await redis.hexists(key, lastdays)
+			? parsedData = JSON.parse(await redis.hget(key, lastdays))
+			: parsedData = JSON.parse(await redis.hget(key, 'data'));
 		const numericalStats = (element) => ({ ...element, deaths: parseInt(element.deaths), cases: parseInt(element.cases), updated: Date.now() });
 		parsedData = parsedData.map(numericalStats);
 	} catch (err) {
