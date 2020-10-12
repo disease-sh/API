5dcaa81ec8fd711af8a01dcd47ba5098526f2936
Julian Pufler <puf17640@spengergasse.at>
2020-10-12 08:09:42 +0200

    0 to null

--- scrapers/covid-19/govScrapers/getCanada.js
+++ scrapers/covid-19/govScrapers/getCanada.js
@@ -20,20 +20,20 @@ const filterByDate = (csv) => {
 const canadaData = async () => {
 	try {
 		const canadaRes = (await axios.get('https://health-infobase.canada.ca/src/data/covidLive/covid19.csv')).data;
-		const parsedCanadaData = await csvUtils.parseCsvData(canadaRes);
+		const parsedCanadaData = csvUtils.parseCsvData(canadaRes);
 		return parsedCanadaData.map(province => ({
 			updated: Date.now(),
 			province: province.prname === 'Canada' ? 'Total' : province.prname,
 			date: province.date,
-			todayCases: parseInt(province.numtoday) || 0,
-			todayTests: parseInt(province.numtestedtoday) || 0,
-			todayRecovered: parseInt(province.numrecoveredtoday) || 0,
-			todayDeaths: parseInt(province.numdeathstoday) || 0,
-			cases: parseInt(province.numconf) || 0,
-			active: parseInt(province.numactive) || 0,
-			tests: parseInt(province.numtested) || 0,
-			recovered: parseInt(province.numrecover) || 0,
-			deaths: parseInt(province.numdeaths) || 0
+			todayCases: parseInt(province.numtoday) || null,
+			todayTests: parseInt(province.numtestedtoday) || null,
+			todayRecovered: parseInt(province.numrecoveredtoday) || null,
+			todayDeaths: parseInt(province.numdeathstoday) || null,
+			cases: parseInt(province.numconf) || null,
+			active: parseInt(province.numactive) || null,
+			tests: parseInt(province.numtested) || null,
+			recovered: parseInt(province.numrecover) || null,
+			deaths: parseInt(province.numdeaths) || null
 		}));
 	} catch (err) {
 		logger.err('Error: Requesting Canada Gov Data failed!', err);
