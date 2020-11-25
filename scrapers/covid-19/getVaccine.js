0ddb5640d0328f2712e18d0a46208d70b0fdfae0
ebwinters@comcast.net <ebwinters@comcast.net>
2020-11-25 08:30:55 -0500

    another shot

--- scrapers/covid-19/getVaccine.js
+++ scrapers/covid-19/getVaccine.js
@@ -28,30 +28,17 @@ const cleanData = (data) => {
  * @param 	{Object} 	redis 	 Redis instance
  */
 const getVaccineData = async (keys, redis) => {
-	let day, month, year;
-	try {
-		const cookieJar = new tough.CookieJar();
-		const html = cheerio.load((await axios.get('https://www.raps.org/news-and-articles/news-articles/2020/3/covid-19-vaccine-tracker', {
-			jar: cookieJar,
-			withCredentials: true,
-			headers: {
-				'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
-				'Accept-Language': 'en-us',
-				'Content-Language': 'en-us'
-			}
-		})).data);
-		const date = html(`.small:first-of-type`).text().split(' ').slice(1, 4);
-		[day, month, year] = date;
-	} catch (err) {
-		logger.err('Error: Requesting vaccine data failed!', err);
-	}
+	const dateObj = new Date();
+	const month = dateObj.getUTCMonth() + 1;
+	const day = dateObj.getUTCDate();
+	const year = dateObj.getUTCFullYear();
+
 	let dataExists = false;
 	let counter = 0;
 	do {
 		try {
 			const cookieJar = new tough.CookieJar();
-			console.log(`https://www.raps.org/RAPS/media/news-images/data/${year}${months[month]}${(day - counter).toString().padStart(2, '0')}-vax-tracker-Craven.csv`);
-			const { data } = await axios.get(`https://www.raps.org/RAPS/media/news-images/data/${year}${months[month]}${(day - counter).toString().padStart(2, '0')}-vax-tracker-Craven.csv`, {
+			const { data } = await axios.get(`https://www.raps.org/RAPS/media/news-images/data/${year}${month}${(day - counter).toString().padStart(2, '0')}-vax-tracker-Craven.csv`, {
 				jar: cookieJar,
 				withCredentials: true,
 				headers: {
@@ -72,7 +59,7 @@ const getVaccineData = async (keys, redis) => {
 			logger.err('Error: Requesting vaccine CSV data failed!', err);
 		}
 		counter++;
-	} while (dataExists === false && counter < 3);
+	} while (dataExists === false && counter < 30);
 };
 
 module.exports = getVaccineData;
