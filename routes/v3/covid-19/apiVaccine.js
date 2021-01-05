adee37c85724c924513d81b393c68a8a64975eeb
nibble0101 <mj.mawa@yahoo.com>
2021-01-05 18:29:32 +0300

    Started working on extending API to include vaccine data

--- routes/v3/covid-19/apiVaccine.js
+++ routes/v3/covid-19/apiVaccine.js
@@ -11,4 +11,12 @@ router.get('/v3/covid-19/vaccine', async (req, res) => {
 	}
 });
 
+router.get('/v3/covid-19/vaccine/coverage', async (req, res) => {
+	const data = JSON.parse(await redis.get(keys.vaccine_coverage));
+	if (data) {
+		res.send(data);
+	} else {
+		res.status(404).send({ message: `Error fetching vaccine coverage data` });
+	}
+});
 module.exports = router;
--- scrapers/covid-19/getVaccine.js
+++ scrapers/covid-19/getVaccine.js
@@ -3,6 +3,7 @@ const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const tough = require('tough-cookie');
 const csv = require('csvtojson');
 const logger = require('../../utils/logger');
+const countries = require('../../utils/countries');
 const { phaseData } = require('../../utils/RAPS');
 
 axiosCookieJarSupport(axios);
@@ -10,7 +11,10 @@ axiosCookieJarSupport(axios);
 const cleanData = (data) => {
 	const htmlRegex = /<(?:.|\n)*?>/gm;
 	const separatorRegex = /;&nbsp;|;/;
-	const listify = (attribute) => attribute.split(separatorRegex).map((sponsor) => sponsor.replace(htmlRegex, '').trim());
+	const listify = (attribute) =>
+		attribute
+			.split(separatorRegex)
+			.map((sponsor) => sponsor.replace(htmlRegex, '').trim());
 	return data.map((trial) => ({
 		candidate: trial.Candidate,
 		mechanism: trial.Mechanism,
@@ -20,6 +24,115 @@ const cleanData = (data) => {
 		institutions: listify(trial.Institution)
 	}));
 };
+
+const groupVaccineDataByCountry = (vaccineData) => {
+	const vaccineDataObject = {},
+		worldVaccineData = {},
+		otherJurisdictions = {};
+	vaccineData.forEach((timelineData) => {
+		// For jurisidctions like Wales, Scotland e.t.c which don't have iso3 code
+		if (timelineData.iso_code === '') {
+			if (otherJurisdictions[timelineData.iso_code] === undefined) {
+				otherJurisdictions[timelineData.iso_code] = [timelineData];
+				return;
+			}
+			otherJurisdictions[timelineData.iso_code].push(timelineData);
+			return;
+		}
+
+		// For world aggregate
+
+		if (timelineData.iso_code === 'OWID_WRL') {
+			if (worldVaccineData[timelineData.iso_code] === undefined) {
+				worldVaccineData[timelineData.iso_code] = [timelineData];
+				return;
+			}
+			worldVaccineData[timelineData.iso_code].push(timelineData);
+			return;
+		}
+
+		// Countries with iso3 code
+
+		if (vaccineDataObject[timelineData.iso_code] === undefined) {
+			vaccineDataObject[timelineData.iso_code] = [timelineData];
+			return;
+		}
+		vaccineDataObject[timelineData.iso_code].push(timelineData);
+	});
+	return { vaccineDataObject, worldVaccineData, otherJurisdictions };
+};
+
+const generateCountrySpecificVaccineData = (timelineData) => {
+	const timeline = {};
+	// All countries have missing value for daily_vaccinations field on the first day. This could be intentional omission
+	// or they are participants in medical trial. Needs to be cross checked.
+	/* eslint-disable */
+  const { total_vaccinations, daily_vaccinations } = timelineData[0];
+  if (daily_vaccinations === "") {
+    timelineData[0].daily_vaccinations = total_vaccinations;
+  }
+  timelineData.forEach((timelineObject) => {
+    const {
+      total_vaccinations,
+      daily_vaccinations,
+      total_vaccinations_per_hundred,
+      daily_vaccinations_per_million,
+    } = timelineObject;
+    if (daily_vaccinations === "" || total_vaccinations === "") return;
+    timeline[timelineObject.date] = {
+      total_vaccinations: parseInt(total_vaccinations),
+      daily_vaccinations: parseInt(daily_vaccinations),
+      total_vaccinations_per_hundred:
+        total_vaccinations_per_hundred === ""
+          ? 0
+          : parseInt(total_vaccinations_per_hundred),
+      daily_vaccinations_per_million:
+        daily_vaccinations_per_million === ""
+          ? 0
+          : parseInt(daily_vaccinations_per_million),
+    };
+  });
+  return { country: "", countryInfo: null, timeline };
+};
+/* eslint-enable */
+/**
+ * Fills redis with vaccinination coverage data
+ * @param 	{string} 	keys	 Redis keys
+ * @param 	{Object} 	redis 	 Redis instance
+ */
+
+async function getVaccineCoverageData(keys, redis) {
+	try {
+		const data = await axios.get(
+			'https://covid.ourworldindata.org/data/vaccinations/vaccinations.csv'
+		);
+		const parsedData = await csv({
+			noheader: false,
+			output: 'json'
+		}).fromString(data.data);
+
+		const vaccineCoverageData = [];
+		const { vaccineDataObject } = groupVaccineDataByCountry(parsedData);
+
+		Object.keys(vaccineDataObject).forEach((iso3CountryCode) => {
+			const countryMetaData = countries.find(
+				(metaData) => metaData.iso3 === iso3CountryCode
+			);
+			const countryVaccineData = generateCountrySpecificVaccineData(
+				vaccineDataObject[iso3CountryCode]
+			);
+
+			if (countryMetaData) {
+				countryVaccineData.country = countryMetaData.country;
+				countryVaccineData.countryInfo = countryMetaData;
+				vaccineCoverageData.push(countryVaccineData);
+			}
+		});
+		redis.set(keys.vaccine_coverage, JSON.stringify(vaccineCoverageData));
+	} catch (error) {
+		logger.err('Error: Requesting vaccine coverage data failed!', error);
+	}
+}
 
 /**
  * Fills redis with vaccine data
@@ -35,24 +148,34 @@ const getVaccineData = async (keys, redis) => {
 	let counter = 0;
 	do {
 		try {
-			const dateString = `${year}${month}${counter.toString().padStart(2, '0')}`;
+			const dateString = `${year}${month}${counter
+				.toString()
+				.padStart(2, '0')}`;
 			const cookieJar = new tough.CookieJar();
-			const { data } = await axios.get(`https://www.raps.org/RAPS/media/news-images/data/${dateString}-vax-tracker-Craven.csv`, {
-				jar: cookieJar,
-				withCredentials: true,
-				headers: {
-					'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
-					'Accept-Language': 'en-us',
-					'Content-Language': 'en-us'
+			const { data } = await axios.get(
+				`https://www.raps.org/RAPS/media/news-images/data/${dateString}-vax-tracker-Craven.csv`,
+				{
+					jar: cookieJar,
+					withCredentials: true,
+					headers: {
+						'User-Agent':
+              'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+						'Accept-Language': 'en-us',
+						'Content-Language': 'en-us'
+					}
 				}
-			});
+			);
 			const parsedData = await csv().fromString(data);
-			redis.set(keys.vaccine, JSON.stringify({
-				source: 'https://www.raps.org/news-and-articles/news-articles/2020/3/covid-19-vaccine-tracker',
-				totalCandidates: parsedData.length.toString(),
-				phases: phaseData(parsedData),
-				data: cleanData(parsedData)
-			}));
+			redis.set(
+				keys.vaccine,
+				JSON.stringify({
+					source:
+            'https://www.raps.org/news-and-articles/news-articles/2020/3/covid-19-vaccine-tracker',
+					totalCandidates: parsedData.length.toString(),
+					phases: phaseData(parsedData),
+					data: cleanData(parsedData)
+				})
+			);
 			dataExists = true;
 		} catch (err) {
 			logger.err('Error: Requesting vaccine CSV data failed!', err);
@@ -61,4 +184,4 @@ const getVaccineData = async (keys, redis) => {
 	} while (dataExists === false && counter < 30);
 };
 
-module.exports = getVaccineData;
+module.exports = { getVaccineData, getVaccineCoverageData };
