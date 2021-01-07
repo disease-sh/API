1bba3905b7a5f8c57380adb6c74ebdb696a33574
nibble0101 <mj.mawa@yahoo.com>
2021-01-07 22:18:25 +0300

    Added routes for fetching vaccine coverage data

--- routes/v3/covid-19/apiVaccine.js
+++ routes/v3/covid-19/apiVaccine.js
@@ -1,12 +1,7 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 const { redis, keys } = require('../../instances');
-
-// This ensures date format conforms with date returned from other endpoints
-const formatVaccineDate = (date) => {
-	const { 0: year, 1: month, 2: day } = date.split('-');
-	return `${month}/${day}/${year.slice(-2)}`;
-};
+const { getLastDays } = require('../../../utils/stringUtils');
 
 router.get('/v3/covid-19/vaccine', async (req, res) => {
 	const data = JSON.parse(await redis.get(keys.vaccine));
@@ -21,9 +16,11 @@ router.get('/v3/covid-19/vaccine/coverage', async (req, res) => {
 	const data = JSON.parse(await redis.get(keys.vaccine_coverage));
 	if (data) {
 		const { world } = data;
+		const { lastdays } = req.query;
+		const numericLastDays = getLastDays(lastdays);
 		const extractedData = {};
-		Object.keys(world).forEach((date) => {
-			extractedData[formatVaccineDate(date)] = world[date].total;
+		Object.keys(world).slice(numericLastDays * -1).forEach((date) => {
+			extractedData[date] = world[date].total;
 		});
 		res.send(extractedData);
 	} else {
@@ -35,13 +32,13 @@ router.get('/v3/covid-19/vaccine/coverage/countries', async (req, res) => {
 	const data = JSON.parse(await redis.get(keys.vaccine_coverage));
 	if (data) {
 		const { countries } = data;
+		const { lastdays } = req.query;
+		const numericLastDays = getLastDays(lastdays);
 		const extractedData = countries.map((country) => {
 			const { timeline } = country;
-			let total = 0;
 			const obj = { country: country.country, timeline: {} };
-			Object.keys(timeline).forEach((date) => {
-				total += timeline[date].daily;
-				obj.timeline[formatVaccineDate(date)] = total;
+			Object.keys(timeline).slice(numericLastDays * -1).forEach((date) => {
+				obj.timeline[date] = timeline[date].total;
 			});
 			return obj;
 		});
@@ -51,37 +48,33 @@ router.get('/v3/covid-19/vaccine/coverage/countries', async (req, res) => {
 	}
 });
 
-router.get(
-	'/v3/covid-19/vaccine/coverage/countries/:country',
-	async (req, res) => {
-		const { country } = req.params;
-		const data = JSON.parse(await redis.get(keys.vaccine_coverage));
-		if (data) {
-			const { countries } = data;
-			const countryVaccineData = countries.find(
-				(countryData) =>
-					countryData.country.toLowerCase() === country.toLowerCase()
-			);
-			if (countryVaccineData) {
-				const { timeline } = countryVaccineData;
-				const obj = { country: countryVaccineData.country, timeline: {} };
-				let total = 0;
-				Object.keys(timeline).forEach((date) => {
-					total += timeline[date].daily;
-					obj.timeline[formatVaccineDate(date)] = total;
-				});
-				res.send(obj);
-			} else {
-				res
-					.status(404)
-					.send({
-						message: `No vaccine data for requested country or country doesn not exist`
-					});
-			}
+router.get('/v3/covid-19/vaccine/coverage/countries/:country', async (req, res) => {
+	const { country } = req.params;
+	const data = JSON.parse(await redis.get(keys.vaccine_coverage));
+	if (data) {
+		const { countries } = data;
+		const countryVaccineData = countries.find((countryData) =>
+			countryData.countryInfo.country.toLowerCase() === country.toLowerCase()
+			|| countryData.countryInfo.iso2.toLowerCase() === country.toLowerCase()
+			|| countryData.countryInfo.iso3.toLowerCase() === country.toLowerCase());
+		if (countryVaccineData) {
+			const { timeline } = countryVaccineData;
+			const { lastdays } = req.query;
+			const numericLastDays = getLastDays(lastdays);
+			const obj = { country: countryVaccineData.country, timeline: {} };
+			Object.keys(timeline).slice(numericLastDays * -1).forEach((date) => {
+				obj.timeline[date] = timeline[date].total;
+			});
+			res.send(obj);
 		} else {
-			res.status(404).send({ message: `Error fetching vaccine coverage data` });
+			res
+				.status(404)
+				.send({ message: 'No vaccine data for requested country or country does not exist' });
 		}
+	} else {
+		res.status(404).send({ message: 'Error fetching vaccine coverage data' });
 	}
+}
 );
 
 module.exports = router;
