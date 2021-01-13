11f8afdd889520afbf523a939250f1cbad9398f9
ebwinters@comcast.net <ebwinters@comcast.net>
2021-01-13 13:06:36 -0500

    change another file

--- scrapers/influenza/getCDC.js
+++ scrapers/influenza/getCDC.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-
 const cheerio = require('cheerio');
 const logger = require('../../utils/logger');
 const { getCurrentWeek } = require('../../utils/dateTimeUtils');
