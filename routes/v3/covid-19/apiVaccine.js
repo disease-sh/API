79226d2ef8070ddf180e1f68bec36701735a8e86
Cristian H Calderon <cristianhcalderon@gmail.com>
2021-01-18 21:51:59 +0000

    change how data vaccine is returned

--- routes/v3/covid-19/apiVaccine.js
+++ routes/v3/covid-19/apiVaccine.js
@@ -61,10 +61,13 @@ router.get('/v3/covid-19/vaccine/coverage/countries/:country', async (req, res) 
 			const { timeline } = countryVaccineData;
 			const { lastdays } = req.query;
 			const numericLastDays = getLastDays(lastdays);
-			const obj = { country: countryVaccineData.country, timeline: {} };
-			Object.keys(timeline).slice(numericLastDays * -1).forEach((date) => {
-				obj.timeline[date] = timeline[date].total;
-			});
+            const obj = { country: countryVaccineData.country, data: [] };
+            Object.keys(timeline).slice(numericLastDays * -1).forEach((date) => {
+                entry = {
+                    date, ...timeline[date]
+                }
+                obj.data.push(entry);
+            });
 			res.send(obj);
 		} else {
 			res.status(404).send({ message: 'No vaccine data for requested country or country does not exist' });
