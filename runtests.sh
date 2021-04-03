afb31b743a1249de7d87d9660b72686750ef7fec
ebwinters@comcast.net <ebwinters@comcast.net>
2021-04-03 15:01:43 -0400

    fix test script

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,6 @@
-npm run test-single $(cat test_to_run.txt | while read line; do echo $line; done)
+if [ -s test_to_run.txt ]
+then
+    npm run test
+else
+    npm run test-single $(cat test_to_run.txt | while read line; do echo $line; done)
+fi
