5e1eaade8c6c778bef7c97702fc2297cd605ed76
Meir Roth <12494197+meirroth@users.noreply.github.com>
2021-05-08 16:23:02 +0000

    Fix Israel Gov Covid-19 endpoint (#997)
    * Fix Israel Gov endpoint
    
    1. Updated query and refactored age and gender data to include percentages, and stats per time frame (All time, past year, 6 month, 3 months, one month).
    2. healthPersonnel, cityData, and hospitalData are no longer updated so they are set to null.
    3. Updated "Traffic Light Map" program data to include  weeklyChangeInCasesPer10k.
    4. Added comments to make it more clear what the data represents according to source.
    5. Fixed Covid test results data that was out of sync with timeline.
    
    * tests and small fix
    
    * Update getIsrael.js

--- scrapers/covid-19/govScrapers/getIsrael.js
+++ scrapers/covid-19/govScrapers/getIsrael.js
@@ -8,15 +8,13 @@ const params = {
 		{ queryName: 'deadPatientsPerDate' },
 		{ queryName: 'recoveredPerDay' },
 		{ queryName: 'testResultsPerDate' },
-		{ queryName: 'isolatedVerifiedDoctorsAndNurses' },
-		{ queryName: 'contagionDataPerCityPublic' },
-		{ queryName: 'hospitalStatus' },
-		{ queryName: 'infectedByAgeAndGenderPublic', parameters: { ageSections: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] } },
-		{ queryName: 'deadByAgeAndGenderPublic', parameters: { ageSections: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] } },
-		{ queryName: 'breatheByAgeAndGenderPublic', parameters: { ageSections: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] } },
-		{ queryName: 'severeByAgeAndGenderPublic', parameters: { ageSections: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] } },
+		{ queryName: 'infectedByPeriodAndAgeAndGender' },
+		{ queryName: 'deadByPeriodAndAgeAndGender' },
+		{ queryName: 'breatheByPeriodAndAgeAndGender' },
+		{ queryName: 'severeByPeriodAndAgeAndGender' },
 		{ queryName: 'spotlightPublic' },
 		{ queryName: 'spotlightLastupdate' }
+
 	]
 };
 
@@ -26,9 +24,22 @@ const params = {
  * @returns {Object}			Cleaned and parsed data to be displayed in API
  */
 const parseData = (data) => {
-	const labelAgeGroup = (el) => el.data.map((entry, i) => {
-		const { section, male, female } = entry;
-		return { section: i === el.data.length - 1 ? `+${section}` : `${section} - ${section + 9}`, male, female };
+	// filter out data by time frame and remove  title
+	const getAgeGroupData = (el, periodName) => el.data
+		.filter(section => section.period === periodName)
+		.map((entry) => {
+			const { section, male, female } = entry;
+			return { section, male, female };
+		});
+	const byAgeAndGender = {};
+	['cases', 'deaths', 'onVentilators', 'critical'].forEach((type, i) => {
+		byAgeAndGender[type] = {
+			allTime: getAgeGroupData(data[5 + i], 'מתחילת קורונה'),
+			oneYear: getAgeGroupData(data[5 + i], 'שנה'),
+			sixMonths: getAgeGroupData(data[5 + i], '6 חודשים'),
+			threeMonths: getAgeGroupData(data[5 + i], '3 חודשים'),
+			oneMonth: getAgeGroupData(data[5 + i], 'חודש אחרון')
+		};
 	});
 	const translateColor = (color) => {
 		switch (true) {
@@ -43,8 +54,8 @@ const parseData = (data) => {
 	const patientsPerDay = data[1].data;
 	const deadPatientsPerDay = data[2].data;
 	const recoveredPerDay = data[3].data.splice(20, data[3].data.length - 1);
-	const testsPerDay = data[4].data.splice(30, data[4].data.length - 1);
-	const staffData = data[5].data;
+	const testsPerDay = data[4].data.splice(11, data[4].data.length - 1);
+	const isTimelineSynced = patientsPerDay[patientsPerDay.length - 1].date.match(dateRegex)[0] === testsPerDay[testsPerDay.length - 1].date.match(dateRegex)[0];
 	const timeline = patientsPerDay.map((elem, index) => ({
 		date: elem.date.match(dateRegex)[0],
 		newHospitalized: elem.new_hospitalized,
@@ -55,42 +66,39 @@ const parseData = (data) => {
 		standardOccupancy: elem.StandardOccupancy,
 		newDeaths: deadPatientsPerDay[index].amount,
 		newlyRecovered: recoveredPerDay[index].amount,
-		newTotalTestsTaken: testsPerDay[index].amount,
-		newVirusTestsTaken: testsPerDay[index].amountVirusDiagnosis,
-		newPositiveTests: testsPerDay[index].positiveAmount,
+		newTotalTestsTaken: isTimelineSynced ? testsPerDay[index].amount : null,
+		newVirusTestsTaken: isTimelineSynced ? testsPerDay[index].amountVirusDiagnosis : null,
+		newPositiveTests: isTimelineSynced ? testsPerDay[index].positiveAmount : null,
 		activeNoncritical: elem.CountEasyStatus,
 		activeModerate: elem.CountMediumStatus,
-		activeCritical: elem.CountHardStatus,
+		// Total active patients in Serious (including critical) condition hospitalized
+		activeSerious: elem.CountHardStatus,
+		// Total active patients in Critical condition hospitalized
+		activeCritical: elem.CountCriticalStatus,
+		// Number of patients on ventilators in hospitals
 		onVentilators: elem.CountBreath
 	}));
 	return {
 		updated: new Date(data[0].data.lastUpdate).valueOf(),
 		data: {
-			byAgeAndGender: {
-				cases: labelAgeGroup(data[8]),
-				deaths: labelAgeGroup(data[9]),
-				onVentilators: labelAgeGroup(data[10]),
-				critical: labelAgeGroup(data[11])
-			},
-			healthPersonnel: {
-				verifiedDoctors: null,
-				verifiedNurses: null,
-				isolatedDoctors: staffData[0].amount,
-				isolatedNurses: staffData[1].amount,
-				isolatedOtherSector: staffData[2].amount
-			},
-			cityData: data[6].data,
-			hospitalData: data[7].data,
+			byAgeAndGender: byAgeAndGender,
+			healthPersonnel: null,
+			cityData: null,
+			hospitalData: null,
 			trafficLightMap: {
-				cityData: data[12].data.map((city) => ({
+				cityData: data[9].data.map((city) => ({
 					name: city.name,
 					score: city.score,
 					color: translateColor(city.color),
+					// The number of new patients in the last week per 10,000 persons
 					newCasesPer10kLastWeek: city.sickTo10000,
+					// (decimal number) The percentage of positive tests out of the total tests for the virus performed in the last week in the locality
 					pctPositiveTestsLastWeek: city.positiveTests,
+					// (decimal number) Percentage change in the number of new patients in the last week compared to the previous week in the locality.
+					weeklyChangeInCasesPer10k: city.growthLastWeek,
 					activeCases: city.activeSick
 				})),
-				lastUpdate: new Date(data[13].data).valueOf()
+				lastUpdate: new Date(data[10].data).valueOf()
 			},
 			timeline
 		}
